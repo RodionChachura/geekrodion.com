d5c7bbdeb1a559f62af844e077eeb55319f129fa
rodion <rodionaaafgh@gmail.com>
2018-08-13 10:00:26 +0300

    update: only virginia certificate

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,24 +10,6 @@ resource "aws_route53_zone" "route_zone" {
   name = "${var.domain}"
 }
 
-resource "aws_acm_certificate" "domain" {
-  domain_name = "${var.domain}"
-  validation_method = "DNS"
-}
-
-resource "aws_route53_record" "cert_validation" {
-  name = "${aws_acm_certificate.domain.domain_validation_options.0.resource_record_name}"
-  type = "${aws_acm_certificate.domain.domain_validation_options.0.resource_record_type}"
-  records = ["${aws_acm_certificate.domain.domain_validation_options.0.resource_record_value}"]
-  zone_id = "${aws_route53_zone.route_zone.zone_id}"
-  ttl = 60
-}
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn = "${aws_acm_certificate.domain.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
-}
-
 resource "aws_acm_certificate" "domain_virginia" {
   provider = "aws.virginia"
   domain_name = "${var.domain}"
