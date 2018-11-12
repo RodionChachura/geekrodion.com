b3a2798821214aa2ff76e4620323d326f371cd0a
rodion <rodionaaafgh@gmail.com>
2018-11-12 20:21:12 +0300

    feature: CodePipeline; refactoring: infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,6 +6,14 @@ provider "aws" {
   alias = "virginia"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "infrastructure-remote-state"
+    key    = "geekrodion/frontend.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 resource "aws_route53_zone" "route_zone" {
   name = "${var.domain}"
 }
@@ -106,5 +114,196 @@ resource "aws_route53_record" "frontend_record" {
     name = "${aws_cloudfront_distribution.frontend.domain_name}"
     zone_id = "${aws_cloudfront_distribution.frontend.hosted_zone_id}"
     evaluate_target_health = false
+  }
+}
+
+
+resource "aws_ecr_repository" "ci" {
+  name = "tf-ci"
+}
+
+resource "aws_ecr_repository_policy" "ci-policy" {
+  repository = "${aws_ecr_repository.ci.name}"
+
+  policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "everybody",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "ecr:*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "tf-${var.bucket_name}-pipeline-artifacts"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "tf-${var.bucket_name}-pipeline"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "tf-${var.bucket_name}-pipeline"
+  role = "${aws_iam_role.codepipeline_role.id}"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "tf-${var.bucket_name}-codebuild-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  role        = "${aws_iam_role.codebuild_role.name}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_codebuild_project" "codebuild" {
+  name         = "tf-codebuild-${var.bucket_name}"
+  service_role = "${aws_iam_role.codebuild_role.arn}"
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com/tf-ci:${var.ci_container_name}"
+    type         = "LINUX_CONTAINER"
+
+    environment_variable {
+      name = "AWS_ACCESS_KEY_ID"
+      value = "${var.access_key}"
+    }
+    environment_variable {
+      name = "AWS_SECRET_ACCESS_KEY"
+      value = "${var.secret_key}"
+    }
+    environment_variable {
+      name = "AWS_DEFAULT_REGION"
+      value = "${var.region}"
+    }
+    environment_variable {
+      name = "BUCKET"
+      value = "${var.bucket_name}"
+    }
+    environment_variable {
+      name = "DISTRIBUTION_ID"
+      value = "${aws_cloudfront_distribution.frontend.id}"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+  }
+}
+
+resource "aws_codepipeline" "pipeline" {
+  name     = "tf-${var.bucket_name}-pipeline"
+  role_arn = "${aws_iam_role.codepipeline_role.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.artifacts.bucket}"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source"]
+
+      configuration {
+        Owner      = "${var.repo_owner}"
+        Repo       = "${var.repo_name}"
+        Branch     = "${var.branch}"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name            = "Build"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      input_artifacts = ["source"]
+      version         = "1"
+
+      configuration {
+        ProjectName = "${aws_codebuild_project.codebuild.name}"
+      }
+    }
   }
 }
--- infrastructure/vars.tf
+++ infrastructure/vars.tf
@@ -6,3 +6,25 @@ variable "domain" {
 variable "bucket_name" {
   default = "geekrodion"
 }
+
+variable "region" { }
+
+variable "access_key" { }
+
+variable "secret_key" { }
+
+variable "ci_container_name" {
+  default = "geekrodion"
+}
+
+variable "repo_owner" {
+  default = "RodionChachura"
+}
+
+variable "repo_name" {
+  default = "geekrodion.com"
+}
+
+variable "branch" {
+  default = "master"
+}
