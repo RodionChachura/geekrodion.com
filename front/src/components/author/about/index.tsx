adeaac27feeae13e7b16da6cf9c6db3b9803dbff
Rodion Chachura <geekrodion@gmail.com>
2020-07-12 15:55:26 +0200

    feature: add text

--- front/src/components/author/about/index.tsx
+++ front/src/components/author/about/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 import Text from '../../text'
+import { Container, TextWrapper } from './styles'
 
 const AuthorAbout = () => {
   return (
-    <>
-      <Text>Hey, I'm Rodion 👋</Text>
-      <Text>I develop software, create indie projects, and make content.</Text>
-    </>
+    <Container>
+      <TextWrapper>
+        <Text>Hi, I’m Rodion 👋</Text>
+      </TextWrapper>
+      <Text bold size={34}>I develop software, make content, and create indie projects</Text>
+      <TextWrapper>
+        <Text>I strive to stay open-minded, so my interests change over time. Right now, I’m fascinated with the idea of bootstrapped SAAS, how to balance productivity with creativity, and being comfortable with myself.</Text>
+      </TextWrapper>
+    </Container>
   )
 }
 
---
+++ front/src/components/author/about/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  flex: 1;
+  margin-right: 40px;
+`
+
+export const TextWrapper = styled.div`
+  margin: 10px 0;
+`
