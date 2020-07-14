a4de3019698967c0f18bd5292554f8d901843da0
Rodion Chachura <geekrodion@gmail.com>
2020-07-14 20:15:21 +0200

    feature: title and description

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rodion Chachura Website`,
+    description: `Hi! I'm Rodion Chachura, also known as geekrodion. You may know me as a creator of increaser.org and booksconcepts.com.`,
+    author: `Rodion Chachura`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 
+import SEO from '../components/seo'
 import Layout from '../components/layout'
 import Author from '../components/author'
 
 const IndexPage = () => {
   return (
     <Layout>
+      <SEO/>
       <Author/>
     </Layout>
   )
