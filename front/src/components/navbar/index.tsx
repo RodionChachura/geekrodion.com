8d6f1e2cce89863b032f0db6bc9e72eb82dac4e2
Rodion Chachura <geekrodion@gmail.com>
2020-07-17 19:20:44 +0200

    workaround: gatsby tags bug

--- front/src/components/navbar/index.tsx
+++ front/src/components/navbar/index.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import { useMediaQuery } from 'beautiful-react-hooks'
 
 import { Placeholder, Container } from './styles'
 import Text from '../text'
-import { MOBILE_WIDTH } from '../author/styles'
 
 export const Navbar = () => {
-  const hideNavbar = useMediaQuery(`(max-width: ${MOBILE_WIDTH}px)`)
-  if (hideNavbar) {
-    return null
-  }
-
   return (
     <>
       <Container>
--- front/src/components/navbar/styles.ts
+++ front/src/components/navbar/styles.ts
@@ -1,13 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Placeholder = styled.div`
+const commonStyle = css`
   flex-shrink: 0;
   height: 80px;
   width: inherit;
   background: ${p => p.theme.color.background};
+  @media(max-width: 600px) {
+    display: none;
+  }
+`
+
+export const Placeholder = styled.div`
+ ${commonStyle};
 `
 
 export const Container = styled(Placeholder)`
+  ${commonStyle};
   position: fixed;
   display: flex;
   align-items: center;
