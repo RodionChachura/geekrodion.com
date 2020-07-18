b006ef2fc2d86a9c45dbd12af4ce93a28337ee0b
Rodion Chachura <geekrodion@gmail.com>
2020-07-18 19:47:19 +0200

    update: project frame styles

--- front/src/components/projects/styles.ts
+++ front/src/components/projects/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 import { NAVBAR_HEIGHT } from '../navbar/styles'
 
+const ONE_COLUMN_WIDTH = '900px'
+
 export const Container = styled.div`
   min-height: calc(100vh - ${NAVBAR_HEIGHT}px);
   display: flex;
@@ -9,16 +11,28 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   flex: 1;
-  margin: 20px 0;
   display: flex;
-  flex-direction: column;
+  @media(max-width: ${ONE_COLUMN_WIDTH}) {
+    flex-direction: column;
+  }
 `
 
 export const ProjectFrame = styled.iframe`
   width: 100%;
-  margin: 10px 0;
   flex: 1;
   border: none;
   border-radius: 20px;
   box-shadow: 2px 14px 20px #0C111467;
+  margin: 10px 0;
+  @media(min-width: ${ONE_COLUMN_WIDTH}) {
+    :nth-child(even) {
+      margin-left: 10px;
+    }
+    :nth-child(odd) {
+      margin-right: 10px;
+    }
+  }
+  @media(max-width: ${ONE_COLUMN_WIDTH}) {
+    min-height: calc(100vh - ${NAVBAR_HEIGHT}px);
+  }
 `
