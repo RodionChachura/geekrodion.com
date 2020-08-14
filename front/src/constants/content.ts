1c1e1486c78d4b02f3c20579e17d0d9acbbb1a82
Rodion Chachura <geekrodion@gmail.com>
2020-08-14 19:19:30 +0300

    process: adding images to series

--- front/src/constants/content.ts
+++ front/src/constants/content.ts
@@ -32,6 +32,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1581282000,
     name: 'Snake Game With Rust, JavaScript, and WebAssembly',
+    imageName: 'snake-game-rust.gif',
     sources: [
       getSource(SourceType.UDEMY, 'snake-game-with-rust-javascript-and-webassembly'),
       getSource(SourceType.MEDIUM, 'snake-game-with-rust-javascript-and-webassembly-5e22b357ec7b'),
@@ -41,6 +42,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1566162000,
     name: 'Snake Game with JavaScript',
+    imageName: 'snake-game-js.gif',
     sources: [
       getSource(SourceType.YOUTUBE, 'playlist?list=PLydcsPcXRjewkhbFU6wihNe6D9YfxYEoQ'),
       getSource(SourceType.MEDIUM, 'snake-game-with-javascript-10e0ad9edb52'),
@@ -50,6 +52,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1562274000,
     name: 'Breakout Game with JavaScript, React and SVG',
+    imageName: 'breakout-game-react.gif',
     sources: [
       getSource(SourceType.YOUTUBE, 'playlist?list=PLydcsPcXRjewg798yVulQmoblrQlu46iM'),
       getSource(SourceType.MEDIUM, 'breakout-game-with-javascript-react-and-svg-c8c62a3d30c6'),
@@ -59,6 +62,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1561150800,
     name: 'Increaser Mindset',
+    imageName: 'increaser-mindset.jpeg',
     sources: [
       getSource(SourceType.MEDIUM, 'increaser-mindset-dc828a2bcd4d'),
     ]
@@ -66,6 +70,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1558904400,
     name: 'Linear Algebra with JavaScript',
+    imageName: 'front/src/images/series/linear-algebra-js.png',
     sources: [
       getSource(SourceType.MEDIUM, 'linear-algebra-with-javascript-46c289178c0'),
       getSource(SourceType.GITHUB, 'linear-algebra')
@@ -74,6 +79,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1554670800,
     name: 'Optimization and Operations Research for Programmers',
+    imageName: 'optimization-and-operations-research.png',
     sources: [
       getSource(SourceType.MEDIUM, 'course-optimization-for-programmers-5316572ed69b'),
       getSource(SourceType.GITHUB, 'optimization')
@@ -82,6 +88,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1533243600,
     name: 'Terraform and AWS for Website Hosting',
+    imageName: 'deploy-spa-terraform.png',
     sources: [
       getSource(SourceType.UDEMY, 'terraform-and-aws-for-website-hosting/'),
       getSource(SourceType.MEDIUM, 'deploying-spa-on-aws-with-terraform-358ba2aeaf9b'),
@@ -91,6 +98,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1546376400,
     name: 'Bar Chart with React',
+    imageName: 'bar-chart-react.gif',
     sources: [
       getSource(SourceType.YOUTUBE, 'playlist?list=PLydcsPcXRjeyJuYLRzFzSXZp-tOG98vBF'),
       getSource(SourceType.MEDIUM, 'bar-chart-with-react-3b20b7907633'),
@@ -99,7 +107,8 @@ export const SERIES: Series[] = [
   },
   {
     finishDate: 1541970000,
-    name: 'Blog with ASP.NET Core and React/Redux.',
+    name: 'Blog with ASP.NET Core and React/Redux',
+    imageName: 'blog-asp-react.png',
     sources: [
       getSource(SourceType.MEDIUM, 'blog-with-asp-net-core-and-react-redux-c80857b93cb6'),
       getSource(SourceType.GITHUB, 'simple-blog-back')
@@ -108,6 +117,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1525035600,
     name: 'Probability Theory and Statistics for Programmers',
+    imageName: 'probability-python.jpeg',
     sources: [
       getSource(SourceType.MEDIUM, 'course-probability-theory-and-statistics-for-programmers-353e20202620'),
     ]
@@ -115,6 +125,7 @@ export const SERIES: Series[] = [
   {
     finishDate: 1524776400,
     name: 'How to Start React Project: Best Practices',
+    imageName: 'start-react-project.png',
     sources: [
       getSource(SourceType.UDEMY, 'how-to-start-react-project-best-practices'),
       getSource(SourceType.MEDIUM, 'redux-saga-create-react-app-b6484f704d68'),
