fb97a6d9bbada93862b79f3ba4ac1fd2cad28d79
Rodion Chachura <geekrodion@gmail.com>
2020-09-26 14:29:12 +0300

    feature: intersection observer for projects

--- front/src/components/projects/index.tsx
+++ front/src/components/projects/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useViewportSpy } from 'beautiful-react-hooks'
 
 import { Container, Content, ProjectFrame } from './styles'
 import { EMBEDDED_INCREASER, EMBEDDED_BOOKSCONCEPTS } from '../../constants/links'
 
 const Projects = () => {
+  const ref = useRef()
+  const isVisible = useViewportSpy(ref)
   return (
-    <Container id={'projects'}>
-      <Content>
-        <ProjectFrame src={EMBEDDED_INCREASER}></ProjectFrame>
-        <ProjectFrame src={EMBEDDED_BOOKSCONCEPTS}></ProjectFrame>
-      </Content>
+    <Container ref={ref} id={'projects'}>
+      {isVisible && (
+        <Content>
+          <ProjectFrame src={EMBEDDED_INCREASER}></ProjectFrame>
+          <ProjectFrame src={EMBEDDED_BOOKSCONCEPTS}></ProjectFrame>
+        </Content>
+      )}
     </Container>
   )
 }
